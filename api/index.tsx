e64cb7f73b836f98f81ce9d7baac4a2c85f6fb22
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-03-27 09:57:31 -0400

    handle vercel

--- api/index.tsx
+++ api/index.tsx
@@ -2,6 +2,7 @@ import { Button, Frog } from "frog";
 import "dotenv/config";
 import { neynar } from "frog/hubs";
 import { NeynarAPIClient } from "@neynar/nodejs-sdk";
+import { handle } from "frog/vercel";
 
 export const app = new Frog({
   assetsPath: "/",
@@ -118,3 +119,41 @@ app.frame("/1", async (c) => {
     ],
   })
 })
+
+
+const IPFS_GATEWAYS = ["https://cloudflare-ipfs.com"];
+
+export const uriToHttp = (uri: string): string[] => {
+  try {
+    const protocol = uri.split(":")[0].toLowerCase();
+    switch (protocol) {
+      case "data":
+        return [uri];
+      case "https":
+        return [uri];
+      case "http":
+        return ["https" + uri.substring(4), uri];
+      case "ipfs": {
+        const hash = uri.match(/^ipfs:(\/\/)?(.*)$/i)?.[2];
+        return IPFS_GATEWAYS.map((g) => `${g}/ipfs/${hash}`);
+      }
+      case "ipns": {
+        const name = uri.match(/^ipns:(\/\/)?(.*)$/i)?.[2];
+        return IPFS_GATEWAYS.map((g) => `${g}/ipns/${name}`);
+      }
+      case "ar": {
+        const tx = uri.match(/^ar:(\/\/)?(.*)$/i)?.[2];
+        return [`https://arweave.net/${tx}`];
+      }
+      default:
+        return [];
+    }
+  } catch (e) {
+    console.error(e);
+    return ["", ""];
+  }
+};
+
+
+export const GET = handle(app);
+export const POST = handle(app);
