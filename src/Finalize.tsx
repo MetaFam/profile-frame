e53693e8f52221cd1cded4c8d8a40a980e18674b
dysbulic <dys@dhappy.org>
2024-03-28 00:57:51 -0400

    got a basic flow working 🐅

---
+++ src/Finalize.tsx
@@ -0,0 +1,16 @@
+export const Finalize = () => (
+  <section style={{
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+    fontSize: '35pt',
+    lineHeight: '7',
+    width: '100%',
+    height: '100%',
+  }}>
+    <p style={{ color: 'blue' }}>Done with configuration.</p>
+    <p style={{ textAlign: 'center', color: 'green' }}>Now, visit MetaGame’s site to write your profile to the Ceramic network.</p>
+  </section>
+)
